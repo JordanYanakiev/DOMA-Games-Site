52e462419f08c1be52274a4ca0d44713b0ba823e
JordanYanakiev <jordan.yanakiev@gmail.com>
2022-03-24 11:26:25 +0200

    upload

---
+++ DOMA2Solution/DOMA2/Controllers/GamesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOMA2.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LandOnGreen()
+        {
+            return View();
+        }
+
+        public IActionResult MemoryInventory()
+        {
+            return View();
+        }
+        
+        public IActionResult HowToPlay()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ DOMA2Solution/DOMA2/Controllers/HowToPlayController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOMA2.Controllers
+{
+    public class HowToPlayController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        
+        
+        public IActionResult LandOnGreenHowToPlay()
+        {
+            return View();
+        }
+        
+        public IActionResult MemoryInventoryHowToPlay()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
